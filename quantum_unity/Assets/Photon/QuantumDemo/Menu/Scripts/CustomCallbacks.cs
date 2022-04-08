dadfe3bd7090170b0686148d3691aa3166c6a48c
Alex Lepsa <lepsalex@gmail.com>
2022-04-08 17:32:12 -0400

    small cleanup

--- quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
+++ quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
@@ -10,17 +10,20 @@ public class CustomCallbacks : QuantumCallbacks
     // paused on Start means waiting for Snapshot
     if (game.Session.IsPaused) return;
 
-    foreach (var lp in game.GetLocalPlayers()) {
-      Debug.Log("CustomCallbacks - sending player: " + lp);
-      game.SendPlayerData(lp, PlayerData);
-    }
+    SendPlayerData(game);
   }
 
   public override void OnGameResync(Quantum.QuantumGame game)
   {
     Debug.Log("Detected Resync. Verified tick: " + game.Frames.Verified.Number);
-    
-    foreach (var lp in game.GetLocalPlayers()) {
+
+    SendPlayerData(game);
+  }
+  
+  private void SendPlayerData(QuantumGame game)
+  {
+    foreach (var lp in game.GetLocalPlayers())
+    {
       Debug.Log("CustomCallbacks - sending player: " + lp);
       game.SendPlayerData(lp, PlayerData);
     }
