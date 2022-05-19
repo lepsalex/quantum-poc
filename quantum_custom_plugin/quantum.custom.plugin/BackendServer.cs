208ec3952a7b08efc61c3e8439b6eb2d33cbf486
Alex <lepsalex@gmail.com>
2022-05-19 12:01:53 -0400

    update plugin to "auth" requests

--- quantum_custom_plugin/quantum.custom.plugin/BackendServer.cs
+++ quantum_custom_plugin/quantum.custom.plugin/BackendServer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Newtonsoft.Json;
@@ -10,9 +11,13 @@ namespace Quantum
   {
     public const string RootUrl = "http://localhost:8080";
     public const string RoomUrl = RootUrl + "/room";
+    public const string AuthUrl = RootUrl + "/auth";
     public const string DemoRoomName = "alex-demo";
 
-    public HttpRequest roomSaveRequest(RoomState roomState, HttpRequestCallback onResponseCallback)
+    // for demo only, would come from env
+    public const string serverAuthToken = "server";
+
+    public HttpRequest RoomSaveRequest(RoomState roomState, HttpRequestCallback onResponseCallback)
     {
       var stream = new MemoryStream();
       var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(roomState));
@@ -22,6 +27,7 @@ namespace Quantum
       {
         Async = false,
         Url = RoomUrl,
+        CustomHeaders = makeAuthHeaders(serverAuthToken),
         Method = "POST",
         ContentType = "application/json",
         DataStream = stream,
@@ -29,15 +35,38 @@ namespace Quantum
       };
     }
 
-    public HttpRequest roomRestoreRequest(string roomId, HttpRequestCallback onResponseCallback)
+    public HttpRequest RoomRestoreRequest(string userAuthToken, string roomId, HttpRequestCallback onResponseCallback)
     {
       return new HttpRequest()
       {
         Async = false,
         Url = $"{RoomUrl}/{roomId}",
+        CustomHeaders = makeAuthHeaders(userAuthToken),
         Method = "GET",
         ContentType = "application/json",
         Callback = onResponseCallback
+      };
+    }
+
+    public HttpRequest RoomAuthRequest(string userAuthToken, string roomId, HttpRequestCallback onResponseCallback)
+    {
+      return new HttpRequest()
+      {
+        Async = false,
+        Url = $"{AuthUrl}/room/${roomId}",
+        CustomHeaders = makeAuthHeaders(userAuthToken),
+        Method = "POST",
+        ContentType = "application/json",
+        Callback = onResponseCallback
+      };
+    }
+
+    // demo method only, would be replaced with a real auth header
+    private Dictionary<string, string> makeAuthHeaders(string accountId)
+    {
+      return new Dictionary<string, string>()
+      {
+        {"X-Fake-Auth", accountId}
       };
     }
   }
--- quantum_custom_plugin/quantum.custom.plugin/CustomQuantumPlugin.cs
+++ quantum_custom_plugin/quantum.custom.plugin/CustomQuantumPlugin.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Newtonsoft.Json;
 using Photon.Deterministic;
@@ -24,42 +26,87 @@ namespace Quantum
 
     public override void OnCreateGame(ICreateGameCallInfo info)
     {
-      // should use pluginHost.PluginHost.GameId instead of BackendServer.DemoRoomName but need to work on unity side for that
+      // in the real appm we wll use custom auth that will give us access to
+      // info.authCookie which we can pass to the backend again to auth room join
+      // ref: https://doc.photonengine.com/en-us/pun/current/connection-and-authentication/authentication/custom-authentication
+      var userAuthToken = info.Nickname;
+
+      // should use PluginHost.GameId instead of BackendServer.DemoRoomName but need to work on unity side for that
       var roomId = BackendServer.DemoRoomName;
 
       // make the http request with the provided callback that defers continuing until after the data is loaded
-      var roomRestoreRequest = _backendServer.roomRestoreRequest(roomId, OnCreateGameCallback);
+      var roomRestoreRequest = _backendServer.RoomRestoreRequest(userAuthToken, roomId, OnCreateGameCallback);
       PluginHost.HttpRequest(roomRestoreRequest, info);
     }
 
     private void OnCreateGameCallback(IHttpResponse response, object state)
     {
-      if (response.Status.Equals(HttpRequestQueueResult.Success))
+      switch (response.HttpCode)
       {
-        // Get PlayerState list from JSON Response
-        var roomState = JsonConvert.DeserializeObject<RoomState>(response.ResponseText);
-        var playerStates = JsonConvert.DeserializeObject<List<PlayerState>>(roomState.playerStates);
+        case 200:
+          Log.Debug("Room restore request successful!");
+          // Get PlayerState list from JSON Response
+          var roomState = JsonConvert.DeserializeObject<RoomState>(response.ResponseText);
+          var playerStates = JsonConvert.DeserializeObject<List<PlayerState>>(roomState.playerStates);
 
-        // Map PlayerState to CommandRestorePlayerState
-        var playerRestoreCommands = playerStates.Select(playerState => new CommandRestorePlayerState()
-        {
-          PlayerRef = playerState.PlayerRef,
-          PlayerPrototype = playerState.PlayerPrototype,
-          PlayerX = playerState.PlayerX,
-          PlayerY = playerState.PlayerY,
-          PlayerZ = playerState.PlayerZ
-        });
+          // Map PlayerState to CommandRestorePlayerState
+          var playerRestoreCommands = playerStates.Select(playerState => new CommandRestorePlayerState()
+          {
+            PlayerRef = playerState.PlayerRef,
+            PlayerPrototype = playerState.PlayerPrototype,
+            PlayerX = playerState.PlayerX,
+            PlayerY = playerState.PlayerY,
+            PlayerZ = playerState.PlayerZ
+          });
 
-        // Send commands in sequence
-        foreach (var command in playerRestoreCommands)
-        {
-          _server.StartupCommands.Add(command);
-        }
+          // Send commands in sequence
+          foreach (var command in playerRestoreCommands)
+          {
+            _server.StartupCommands.Add(command);
+          }
+
+          break;
+        case 401:
+          throw new UnauthorizedAccessException("Room restore request unauthorized!");
+        case 403:
+          throw new UnauthorizedAccessException("Room restore request forbidden!");
       }
 
       base.OnCreateGame((ICreateGameCallInfo) response.CallInfo);
     }
 
+    public override void BeforeJoin(IBeforeJoinGameCallInfo info)
+    {
+      // in the real appm we wll use custom auth that will give us access to
+      // info.authCookie which we can pass to the backend again to auth room join
+      // ref: https://doc.photonengine.com/en-us/pun/current/connection-and-authentication/authentication/custom-authentication
+      var userAuthToken = info.Nickname;
+
+      // should use PluginHost.GameId instead of BackendServer.DemoRoomName but need to work on unity side for that
+      var roomId = BackendServer.DemoRoomName;
+
+      var roomAuthRequest = _backendServer.RoomAuthRequest(userAuthToken, roomId, BeforeJoinCallback);
+      PluginHost.HttpRequest(roomAuthRequest, info);
+    }
+
+    private void BeforeJoinCallback(IHttpResponse response, object state)
+    {
+      // copying from base method we are no longer calling
+      Debug.Assert(!this.fireAssert || this.PluginHost != null);
+
+      switch (response.HttpCode)
+      {
+        case 200:
+          response.CallInfo.Continue();
+          break;
+        case 401:
+          response.CallInfo.Fail();
+          break;
+        case 403:
+          response.CallInfo.Fail();
+          break;
+      }
+    }
 
     public override void OnCloseGame(ICloseGameCallInfo info)
     {
@@ -89,13 +136,25 @@ namespace Quantum
       };
 
       // Serialize and send players data to backend
-      var roomSaveRequest = _backendServer.roomSaveRequest(roomState, OnCloseGameCallback);
+      var roomSaveRequest = _backendServer.RoomSaveRequest(roomState, OnCloseGameCallback);
       PluginHost.HttpRequest(roomSaveRequest, info);
     }
 
     private void OnCloseGameCallback(IHttpResponse response, object state)
     {
       // TODO: Handle request issues/retries here
+      switch (response.HttpCode)
+      {
+        case 201:
+          Log.Debug("Room save request successful!");
+          break;
+        case 401:
+          Log.Debug("Room save request unauthorized!");
+          break;
+        case 403:
+          Log.Debug("Room save request forbidden!");
+          break;
+      }
 
       // Dispose server and call base class
       _server.Dispose();
